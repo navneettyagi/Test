030e85a68ab6aedc9a9b9ecae026b8386e70e81f
Ntyagi <Ntyagi@BHAVNAWKS093.BHAVNACORP.NET>
2019-05-03 11:52:54 +0530

    Commit

--- Meridian/src/main/java/com/qa/test/LettersAndDocs.java
+++ Meridian/src/main/java/com/qa/test/LettersAndDocs.java
@@ -7,13 +7,14 @@ import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.junit.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
